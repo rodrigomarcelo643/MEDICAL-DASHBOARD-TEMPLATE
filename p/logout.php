06347f263160149f3be777096cf984715d8cbab6
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2024-08-25 22:44:49 +0800

    Logout Configuration Method

--- p/logout.php
+++ p/logout.php
@@ -1,8 +1,14 @@
 <?php
+//============Logout Configuration Details
 
+//======Starting the main point session
 session_start();
+//========Unsetting the main point session  
 session_unset();
+//==========Destrying the Data or the account being logged in
 session_destroy();
+
+//===Encoding through JSON Format
 echo json_encode(["success" => true]);
 
 
